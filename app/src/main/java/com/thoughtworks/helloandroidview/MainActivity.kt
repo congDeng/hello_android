94ee74ef22300c4ed2e1455de8c50beb37b6dfc3
congDeng <congcong.deng@thoughtworks.com>
2024-03-27 11:03:19 +0800

    constraint layout

--- app/src/main/java/com/thoughtworks/helloandroidview/MainActivity.kt
+++ app/src/main/java/com/thoughtworks/helloandroidview/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.thoughtworks.helloandroidview
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.ViewGroup
 import android.widget.Button
@@ -34,6 +35,13 @@ class MainActivity : AppCompatActivity() {
             )
             layoutParams.setMargins(0,16,0,0)
             button.layoutParams = layoutParams
+
+            if(number == 1){
+                button.setOnClickListener{
+                    val intent = Intent(this@MainActivity, ConstraintActivity::class.java)
+                    startActivity(intent)
+                }
+            }
 
             layout.addView(button)
         }
