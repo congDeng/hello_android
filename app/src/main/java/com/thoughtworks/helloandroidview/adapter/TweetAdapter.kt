2f10fad0710f6626ba14a3f2c5562380847a26ec
congDeng <congcong.deng@thoughtworks.com>
2024-03-30 12:08:49 +0800

    add view type of mo more data

--- app/src/main/java/com/thoughtworks/helloandroidview/adapter/TweetAdapter.kt
+++ app/src/main/java/com/thoughtworks/helloandroidview/adapter/TweetAdapter.kt
@@ -13,6 +13,11 @@ import com.thoughtworks.helloandroidview.model.Tweet
 class TweetAdapter : RecyclerView.Adapter<ViewHolder>() {
     private val tweets = arrayListOf<Tweet>()
 
+    companion object {
+        private const val VIEW_TYPE_ITEM = 0
+        private const val VIEW_TYPE_NO_MORE_DATA = 1
+    }
+
     @SuppressLint("NotifyDataSetChanged")
     fun setTweets(newTweets: ArrayList<Tweet>){
         tweets.clear()
@@ -20,19 +25,42 @@ class TweetAdapter : RecyclerView.Adapter<ViewHolder>() {
         notifyDataSetChanged()
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        return TweetViewHolder(layoutInflater.inflate(R.layout.tweet_item_layout, parent, false))
+        return when (viewType) {
+            VIEW_TYPE_ITEM -> {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                TweetViewHolder(layoutInflater.inflate(R.layout.tweet_item_layout, parent, false))
+            }
+            VIEW_TYPE_NO_MORE_DATA -> {
+                val view = LayoutInflater.from(parent.context).inflate(R.layout.no_more_data_item_layout, parent, false)
+                NoMoreDataViewHolder(view)
+            }
+            else -> throw IllegalArgumentException("Invalid view type")
+        }
+
     }
 
     override fun getItemCount(): Int {
-        return tweets.size
+        return if (tweets.isEmpty()) {
+            0
+        } else {
+            tweets.size + 1
+        }
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val tweet = tweets[position]
-        val tweetViewHolder = holder as TweetViewHolder
-        tweetViewHolder.nicknmae.text = tweet.sender.nick
-        tweetViewHolder.content.text = tweet.content
+        if (holder is TweetViewHolder) {
+            val tweet = tweets[position]
+            holder.nicknmae.text = tweet.sender.nick
+            holder.content.text = tweet.content
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (position < tweets.size) {
+            VIEW_TYPE_ITEM
+        } else {
+            VIEW_TYPE_NO_MORE_DATA
+        }
     }
 
 
@@ -45,5 +73,7 @@ class TweetAdapter : RecyclerView.Adapter<ViewHolder>() {
             content = itemView.findViewById(R.id.content)
         }
     }
+
+    internal class NoMoreDataViewHolder(itemView: View) : ViewHolder(itemView)
 
 }
