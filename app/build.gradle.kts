577d2319b4256ea3abbdc8248372fb9eb621d9db
congDeng <congcong.deng@thoughtworks.com>
2024-04-01 16:57:30 +0800

    add coil lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.gson)
+    implementation(libs.coil)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
